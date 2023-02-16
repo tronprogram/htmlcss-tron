d0151496830ab7b7b51a89c0023aef63f3a2f8d6
tronprogram <60573173+tronprogram@users.noreply.github.com>
2023-02-16 10:25:50 -0500

    Add files via upload

---
+++ da-js-intro/intro.js
@@ -0,0 +1,12 @@
+function Inventario(Nombre){
+    this.nombre = Nombre;
+    this.articulos = []
+}
+Inventario.prototype.getNombre = function(){    f ()
+    return this.Nombre
+    
+}
+
+Inventario.prototype.add = function(articulo, cantidad){    f()
+    this.articulos[articulo] = cantidad;
+}
