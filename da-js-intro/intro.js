b1157225ce9c8d6b410d2d0dac4f38a18f2ebf53
tronprogram <bassgamer1234@outlook.es>
2023-02-18 16:38:34 -0500

    modified:   da-js-intro/intro.js     s

--- da-js-intro/intro.js
+++ da-js-intro/intro.js
@@ -1,12 +1,23 @@
-function Inventario(Nombre){
-    this.nombre = Nombre;
-    this.articulos = []
-}
-Inventario.prototype.getNombre = function(){    f ()
-    return this.Nombre
-    
+class Inventario {
+    constructor(Nombre) {
+        this.nombre = Nombre;
+        this.articulos = [];
+    }
+    getNombre() {
+        f();
+        return this.Nombre;
+
+    }
+    add(articulo, cantidad) {
+        f();
+        this.articulos[articulo] = cantidad;
+    }
+    cantidad(articulo) {
+        return this.articulos[articulo];
+    }
 }
 
-Inventario.prototype.add = function(articulo, cantidad){    f()
-    this.articulos[articulo] = cantidad;
-}
+let libros = new Inventario('libros');
+libros.getNombre()
+libros.add("Aprendiendo JavaScript", 5)
+libros.cantidad("Aprendiendo JavaScript")
